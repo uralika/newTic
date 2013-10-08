36e754e64eabfc3cd40fd7aa41524cdc2e93c557
Anastasiya Sutyagina <asutyagina@gmail.com>
2013-10-08 09:26:58 -0700

    changed array of strings into array of objects, put all images into a separate folder

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,55 +1,76 @@
-/*'use strict';*/
+//'use strict';
 
 angular.module('newTicApp').controller('TicTacCtrl', function ($scope) {
 //var TicTacCtrl = function($scope) {-->
 
-$scope.ticTacToe=[['','',''],
-              ['','',''],
-              ['','','']];
-var playerTurn = 1;
-$scope.clickSquare = function(row,column,cell) {
-  if(playerTurn % 2 ==1)
-    event.target.innerHTML = "<img src='views/tick_tock.jpg'>";
-  else
-    event.target.innerHTML = "<img src='views/web.jpeg'>";
-  ++playerTurn
+$scope.ticTacToe = [[ {val:'', r:0, c:0}, {val:'', r:0, c:1}, {val:'',r:0,c:2} ],
+        [ {val:'', r:1 ,c:0}, {val:'',r:1,c:1}, {val:'',r:1,c:2}],
+        [ {val:'', r:2, c:0}, {val:'', r:2, c:1}, {val:'', r:2, c:2}]];
 
+playerTurn = 1;
 
-somebodyWon = false;
-for(x=0;x<=2;x++)
+$scope.findImg = function(cell) {
+  switch(cell.val)
   {
-  if (this.ticTacToe[0][x] == this.ticTacToe[1][x] &&
-    this.ticTacToe[1][x] == this.ticTacToe[2][x] && 
-    this.ticTacToe[0][x] != "" &&
-    this.ticTacToe[0][x] == "<img src='views/tick_tock.jpg'>")
+    case "X":
+    return "../../images/tick_tock.jpg";
+    case "O":
+    return "../../images/web.jpeg";
+  }
+}
+
+$scope.clickSquare = function(cell) {
+  var tic = $scope.ticTacToe;
+  if(cell.val != "")
+    return;
+  if(playerTurn % 2 == 1) 
+    cell.val = "X";
+  else
+    cell.val = "O";
+   ++playerTurn
+};
+
+$scope.resetBoard = function(){
+    // Simply clear out each cell in the array
+    for(rw in $scope.ticTacToe)
+      for(col in $scope.ticTacToe[rw])
+        $scope.ticTacToe[rw][col].val = "";
+    };
+var somebodyWon = false
+
+for(var c=0;c<=2;++c) {
+
+  if ($scope.ticTacToe[0][c] == $scope.ticTacToe[1][c] &&
+    $scope.ticTacToe[1][c] == $scope.ticTacToe[2][c] && 
+    $scope.ticTacToe[0][c] != "" &&
+    $scope.ticTacToe[0][c] == "../../images/tick_tock.jpg")
     {
-     	alert('hi!');
         document.getElementById("tick_tock").style.display="block";
         somebodyWon = true;
     }
   
-  if (this.ticTacToe[0][x] == this.ticTacToe[1][x] &&
-    this.ticTacToe[1][x] == this.ticTacToe[2][x] && 
-    this.ticTacToe[0][x] != "" &&
-    this.ticTacToe[0][x] == "<img src='views/web.jpeg'>") 
+  if ($scope.ticTacToe[0][c] == $scope.ticTacToe[1][c] &&
+    $scope.ticTacToe[1][c] == $scope.ticTacToe[2][c] && 
+    $scope.ticTacToe[0][c] != "" &&
+    $scope.ticTacToe[0][c] == "../../images/web.jpeg") 
     {   
         document.getElementById("web").style.display="block";
         somebodyWon = true;
    }
 
-  if (this.ticTacToe[x][0] == this.ticTacToe[x][1] &&
-    this.ticTacToe[x][1] == this.ticTacToe[x][2] && 
-    this.ticTacToe[x][0] != "" &&
-    this.ticTacToe[x][0] == "<img src='views/tick_tock.jpg'>")
+  if ($scope.ticTacToe[c][0] == $scope.ticTacToe[c][1] &&
+    $scope.ticTacToe[c][1] == $scope.ticTacToe[c][2] && 
+    $scope.ticTacToe[c][0] != "" &&
+    $scope.ticTacToe[c][0] == "../../images/tick_tock.jpg")
     {
         document.getElementById("tick_tock").style.display="block";
         somebodyWon = true;
     }
  
-  if (this.ticTacToe[x][0] == this.ticTacToe[x][1] &&
-    this.ticTacToe[x][1] == this.ticTacToe[x][2] && 
-    this.ticTacToe[x][0] != "" &&
-    this.ticTacToe[x][0] == "<img src='views/web.jpeg'>")
+  if ($scope.ticTacToe[c][0] == $scope.ticTacToe[c][1] &&
+    $scope.ticTacToe[c][1] == $scope.ticTacToe[c][2] && 
+    $scope.ticTacToe[c][0] != "" &&
+    $scope.ticTacToe[c][0] == "../../images/web.jpeg")
     {
         document.getElementById("web").style.display="block";
         somebodyWon = true;
@@ -57,37 +78,138 @@ for(x=0;x<=2;x++)
 
 
 
-  if(this.ticTacToe[0][0] == this.ticTacToe[1][1] &&
-     this.ticTacToe[1][1] == this.ticTacToe[2][2] &&
-     this.ticTacToe[0][0] != "" &&
-     this.ticTacToe[0][0] == '<img src="tick_tock.jpg">')
+  if($scope.ticTacToe[0][0] == $scope.ticTacToe[1][1] &&
+     $scope.ticTacToe[1][1] == $scope.ticTacToe[2][2] &&
+     $scope.ticTacToe[0][0] != "" &&
+     $scope.ticTacToe[0][0] == "../../images/tick_tock.jpg")
     {
           document.getElementById("tick_tock").style.display="block";
           somebodyWon = true;
       }
 
-   if (this.ticTacToe[0][0] == this.ticTacToe[2][2] &&
-     this.ticTacToe[1][1] == this.ticTacToe[2][2] &&
-     this.ticTacToe[0][0] != "" &&
-     this.ticTacToe[0][0] == '<img src="web.jpeg">')
+   if ($scope.ticTacToe[0][0] == $scope.ticTacToe[2][2] &&
+     $scope.ticTacToe[1][1] == $scope.ticTacToe[2][2] &&
+     $scope.ticTacToe[0][0] != "" &&
+     $scope.ticTacToe[0][0] == "../../images/web.jpeg")
       {
         document.getElementById("web").style.display="block";
         somebodyWon = true;
       }
 
-  if(this.ticTacToe[0][2] == this.ticTacToe[1][1] &&
-     this.ticTacToe[1][1] == this.ticTacToe[2][0] &&
-     this.ticTacToe[0][2] != "" &&
-     this.ticTacToe[0][2] == '<img src="tick_tock.jpg">')
+  if($scope.ticTacToe[0][2] == $scope.ticTacToe[1][1] &&
+     $scope.ticTacToe[1][1] == $scope.ticTacToe[2][0] &&
+     $scope.ticTacToe[0][2] != "" &&
+     $scope.ticTacToe[0][2] == "../../images/tick_tock.jpg")
       {
           document.getElementById("tick_tock").style.display="block";
          somebodyWon = true;
       }
 
-  if (this.ticTacToe[0][2] == this.ticTacToe[1][1] &&
-     this.ticTacToe[1][1] == this.ticTacToe[2][0] &&
-     this.ticTacToe[0][2] != "" &&
-     this.ticTacToe[0][2] == '<img src="web.jpeg">')
+  if ($scope.ticTacToe[0][2] == $scope.ticTacToe[1][1] &&
+     $scope.ticTacToe[1][1] == $scope.ticTacToe[2][0] &&
+     $scope.ticTacToe[0][2] != "" &&
+     $scope.ticTacToe[0][2] == "../../images/web.jpeg")
+    {
+          document.getElementById("web").style.display="block";
+          somebodyWon = true;
+
+    }
+
+  if (!somebodyWon && playerTurn == 10)
+   {
+     document.getElementById("draw").style.display="block";
+   }
+}
+});
+  
+/*
+$scope.ticTacToe=[['','',''],
+              ['','',''],
+              ['','','']];
+var playerTurn = 1;
+$scope.clickSquare = function(row,column) {
+  if(playerTurn % 2 == 1)
+    //$scope.ticTacToe[row][column] = "<img src='tick_tock.jpg'>";
+    event.target.innerHTML = "<img src='views/tick_tock.jpg'>";
+  else
+    //$scope.ticTacToe[row][column] = "<img scr='web.jpeg'>";
+    event.target.innerHTML = "<img src='views/web.jpeg'>";
+  ++playerTurn
+
+somebodyWon = false;
+
+for(x=0;x<=2;x++)
+//for(playerTurn > 4)
+  {
+  if ($scope.ticTacToe[0][x] == $scope.ticTacToe[1][x] &&
+    $scope.ticTacToe[1][x] == $scope.ticTacToe[2][x] && 
+    $scope.ticTacToe[0][x] != "" &&
+    $scope.ticTacToe[0][x] == "<img src='views/tick_tock.jpg'>")
+    {
+        document.getElementById("tick_tock").style.display="block";
+        somebodyWon = true;
+    }
+  
+  if ($scope.ticTacToe[0][x] == $scope.ticTacToe[1][x] &&
+    $scope.ticTacToe[1][x] == $scope.ticTacToe[2][x] && 
+    $scope.ticTacToe[0][x] != "" &&
+    $scope.ticTacToe[0][x] == "<img src='views/web.jpeg'>") 
+    {   
+        document.getElementById("web").style.display="block";
+        somebodyWon = true;
+   }
+
+  if ($scope.ticTacToe[x][0] == $scope.ticTacToe[x][1] &&
+    $scope.ticTacToe[x][1] == $scope.ticTacToe[x][2] && 
+    $scope.ticTacToe[x][0] != "" &&
+    $scope.ticTacToe[x][0] == "<img src='views/tick_tock.jpg'>")
+    {
+        document.getElementById("tick_tock").style.display="block";
+        somebodyWon = true;
+    }
+ 
+  if ($scope.ticTacToe[x][0] == $scope.ticTacToe[x][1] &&
+    $scope.ticTacToe[x][1] == $scope.ticTacToe[x][2] && 
+    $scope.ticTacToe[x][0] != "" &&
+    $scope.ticTacToe[x][0] == "<img src='views/web.jpeg'>")
+    {
+        document.getElementById("web").style.display="block";
+        somebodyWon = true;
+    }
+
+
+
+  if($scope.ticTacToe[0][0] == $scope.ticTacToe[1][1] &&
+     $scope.ticTacToe[1][1] == $scope.ticTacToe[2][2] &&
+     $scope.ticTacToe[0][0] != "" &&
+     $scope.ticTacToe[0][0] == '<img src="tick_tock.jpg">')
+    {
+          document.getElementById("tick_tock").style.display="block";
+          somebodyWon = true;
+      }
+
+   if ($scope.ticTacToe[0][0] == $scope.ticTacToe[2][2] &&
+     $scope.ticTacToe[1][1] == $scope.ticTacToe[2][2] &&
+     $scope.ticTacToe[0][0] != "" &&
+     $scope.ticTacToe[0][0] == '<img src="web.jpeg">')
+      {
+        document.getElementById("web").style.display="block";
+        somebodyWon = true;
+      }
+
+  if($scope.ticTacToe[0][2] == $scope.ticTacToe[1][1] &&
+     $scope.ticTacToe[1][1] == $scope.ticTacToe[2][0] &&
+     $scope.ticTacToe[0][2] != "" &&
+     $scope.ticTacToe[0][2] == '<img src="tick_tock.jpg">')
+      {
+          document.getElementById("tick_tock").style.display="block";
+         somebodyWon = true;
+      }
+
+  if ($scope.ticTacToe[0][2] == $scope.ticTacToe[1][1] &&
+     $scope.ticTacToe[1][1] == $scope.ticTacToe[2][0] &&
+     $scope.ticTacToe[0][2] != "" &&
+     $scope.ticTacToe[0][2] == '<img src="web.jpeg">')
     {
           document.getElementById("web").style.display="block";
           somebodyWon = true;
@@ -102,4 +224,4 @@ for(x=0;x<=2;x++)
 }
 };
 });
-
+*/
