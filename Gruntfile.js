232ac26da76d735bae89d5da7944e05b445e6aae
Anastasiya Sutyagina <asutyagina@gmail.com>
2013-10-11 14:50:38 -0700

    removed dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -240,17 +240,17 @@ module.exports = function (grunt) {
     copy: {
       dist: {
         files: [{
-          expand: true,
-          dot: true,
-          cwd: '<%= yeoman.app %>',
-          dest: '<%= yeoman.dist %>',
-          src: [
-            '*.{ico,png,txt}',
-            '.htaccess',
-            'bower_components/**/*',
-            'images/{,*/}*.{gif,webp}',
-            'styles/fonts/*'
-          ]
+                expand: true,
+                dest: '<%= yeoman.dist %>',
+                cwd: 'heroku',
+                src: '*',
+                rename: function (dest, src) {
+                    var path = require('path');
+                    if (src === 'distpackage.json') {
+                        return path.join(dest, 'package.json');
+                    }
+                    return path.join(dest, src);
+                }
         }, {
           expand: true,
           cwd: '.tmp/images',
