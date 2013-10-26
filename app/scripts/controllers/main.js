cab9cb8fca7c703e6cfbc556e008c23862a8a91b
Anastasiya Sutyagina <asutyagina@gmail.com>
2013-10-26 02:06:25 -0700

    added favicon

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,15 +1,8 @@
-//'use strict';
-
 angular.module('newTicApp').controller('TicTacCtrl', function ($scope, angularFire) {
 
 $scope.room = [];
 $scope.queue = {};
 var playerTurn = 1;
-
-/*$scope.room.ticTacToe = [[ {val:'', r:0, c:0}, {val:'', r:0, c:1}, {val:'',r:0,c:2} ],
-        [ {val:'', r:1 ,c:0}, {val:'', r:1,c:1}, {val:'',r:1,c:2}],
-        [ {val:'', r:2, c:0}, {val:'', r:2, c:1}, {val:'', r:2, c:2}]];*/
-
 
 var room = new Firebase("https://tick-tock-web.firebaseio.com/rooms");
 angularFire(room, $scope, "room").then( function() {
@@ -45,14 +38,7 @@ angularFire(room, $scope, "room").then( function() {
       console.log("Player 2's game is: " + $scope.roomId);    
     }
 
-  }); //? to take out?
-
- //});
-
-/*$scope.hello = function () {
-  $scope.room[$scope.roomId].ticTacToe[0][0] = "Hello!";
-};*/
-//});
+  }); 
 
 $scope.playMove = function (cell) {
   if((!$scope.room[$scope.roomId].waiting) && ($scope.player == $scope.room[$scope.roomId].playerTurn)) {
@@ -71,26 +57,12 @@ $scope.playMove = function (cell) {
 };
 
 
-/*$scope.ticTacToe = [[ {val:'', r:0, c:0}, {val:'', r:0, c:1}, {val:'',r:0,c:2} ],
-        [ {val:'', r:1 ,c:0}, {val:'', r:1,c:1}, {val:'',r:1,c:2}],
-        [ {val:'', r:2, c:0}, {val:'', r:2, c:1}, {val:'', r:2, c:2} ]];*/
-
 
 var playerTurn = 1;
 
 $scope.tick_tockShow = false;
 $scope.webShow = false;
 $scope.drawShow = false;
-
-/*$scope.findImg = function(cell) {
-  switch(cell.val)
-  {
-    case "X":
-    return "images/tick_tock.jpg";
-    case "O":
-    return "images/web.jpeg";
-  }
-};*/
 
 $scope.clickSquare = function(cell) {
  // var tic = $scope.ticTacToe;
@@ -103,6 +75,8 @@ $scope.clickSquare = function(cell) {
    ++$scope.room[$scope.roomId].playerTurn;
 
 var somebodyWon = false;
+
+// win conditions
 
 for(var c=0;c<=2;++c) {
 
